1db43847cdfd87b92fa5dffca28c6564d510e2e0
RAMAZAN DEMİR <rmzn.dmr.info@gmail.com>
2023-11-25 16:53:40 +0300

    Advert service fix

--- src/main/java/com/fasonver/fas10ver/controller/AdvertController.java
+++ src/main/java/com/fasonver/fas10ver/controller/AdvertController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v/advert")
+@RequestMapping("/api/v1/advert")
 @RequiredArgsConstructor
 public class AdvertController {
 
