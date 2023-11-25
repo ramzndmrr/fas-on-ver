80689cced31dd24cad1767dcfca0c47557eb9f00
RAMAZAN DEMİR <rmzn.dmr.info@gmail.com>
2023-11-25 16:51:32 +0300

    Advert service fix

--- src/main/java/com/fasonver/fas10ver/controller/AdvertController.java
+++ src/main/java/com/fasonver/fas10ver/controller/AdvertController.java
@@ -5,6 +5,7 @@ import com.fasonver.fas10ver.service.advert.AdvertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class AdvertController {
     private final AdvertService advertService;
 
     @PostMapping
-    public ResponseEntity<AdvertDto> save(AdvertDto advertDto){
+    public ResponseEntity<AdvertDto> save(@RequestBody AdvertDto advertDto){
         return ResponseEntity.ok(advertService.save(advertDto));
     }
 
